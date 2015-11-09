166a5501aca9627854e90736a33546157d5d0b0c
free-city-of-ulm <cebrown5597@gmail.com>
2015-11-09 00:03:30 -0500

    changed to fish

---
+++ fish/aliases.fish
@@ -0,0 +1,12 @@
+alias ll 'ls -Fl --color=auto'
+alias l 'ls -F --color=auto'
+alias la 'ls -Fa --color=auto'
+alias lla 'ls -Fla --color=auto'
+alias ping 'ping -c3'
+
+function please
+    eval sudo $history[1]
+end
+
+
+alias c 'clear'
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,12 @@
+function fish_prompt
+    set_color red
+    echo -n (hostname -s)
+    set_color normal
+    echo -n "@"
+    set_color blue
+    echo -n "$USER "
+    set_color $fish_color_cwd
+    echo -n $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||' 
+    set_color normal
+    echo -n ' $ '
+end
