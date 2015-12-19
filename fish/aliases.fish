5a73b9b1af62c2d2f4374f6fa356c71333954d93
Christopher Brown <cebrown5597@gmail.com>
2015-12-19 00:16:34 -0500

    fish updates

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -3,10 +3,10 @@ alias l 'ls -F --color=auto'
 alias la 'ls -Fa --color=auto'
 alias lla 'ls -Fla --color=auto'
 alias ping 'ping -c3'
+alias scrot 'scrot -e \'mv $f ~/Pictures/Screenshots/\''
 
 function please
     eval sudo $history[1]
 end
-
 
 alias c 'clear'
