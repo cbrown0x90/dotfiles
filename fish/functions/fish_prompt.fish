0d643dc4424e944356c1cfcf52f10297c912ac02
Christopher Brown <cebrown5597@gmail.com>
2015-12-19 00:21:01 -0500

    fixed prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,10 +1,10 @@
 function fish_prompt
-    set_color red
-    echo -n (hostname -s)
+    set_color blue
+    echo -n "$USER"
     set_color normal
     echo -n "@"
-    set_color blue
-    echo -n "$USER "
+    set_color red
+    echo -n (hostname -s) ""
     set_color $fish_color_cwd
     echo -n $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||' 
     set_color normal
