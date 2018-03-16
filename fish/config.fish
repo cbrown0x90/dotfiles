1ae02bc816e5fb1f9b5a544a20f60ef287b45202
Christopher Brown <cebrown5597@gmail.com>
2018-03-16 10:05:38 -0400

    Changed ENV variables and added virtualfish

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,14 @@
 # cowsay on launch
 fortune -s | cowsay
 
+eval (python -m virtualfish auto_activation)
+
 # modify the path
-set -gx PATH ~/.scripts $PATH /usr/local/bin /usr/sbin /sbin
-set -gx JAVA_HOME /opt/icedtea-bin-3.0.1
+set -gx JAVA_HOME /opt/oracle-jdk-bin-1.8.0
+set -gx PATH ~/.scripts $PATH /usr/local/bin /usr/sbin /sbin $JAVA_HOME/bin
 set -gx ANDROID_SWT /usr/share/swt-3.7/lib
+set -gx VDPAU_DRIVER nouveau
+set -gx VDPAU_DRIVER nouveau
 
 # load the color scheme
 eval sh /home/chris/.config/base16-shell/base16-twilight.dark.sh
