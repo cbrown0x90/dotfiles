9eedd96da26439fb280f1a21ca32ac16d61f30c6
Christopher Brown <cebrown5597@gmail.com>
2018-06-19 11:19:42 -0400

    Update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,17 +1,10 @@
 # cowsay on launch
 fortune -s | cowsay
 
-eval (python -m virtualfish auto_activation)
-
 # modify the path
-set -gx JAVA_HOME /opt/oracle-jdk-bin-1.8.0
-set -gx PATH ~/.scripts $PATH /usr/local/bin /usr/sbin /sbin $JAVA_HOME/bin
-set -gx ANDROID_SWT /usr/share/swt-3.7/lib
+set -gx PATH ~/.scripts $PATH /usr/local/bin /usr/sbin /sbin
 set -gx VDPAU_DRIVER nouveau
 set -gx VDPAU_DRIVER nouveau
-
-# load the color scheme
-eval sh /home/chris/.config/base16-shell/base16-twilight.dark.sh
 
 # remove the fish greeting
 set fish_greeting ""
