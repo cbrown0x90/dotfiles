aa289b65129025b6195bc760a9c93a91893821f7
Christopher Brown <cebrown5597@gmail.com>
2021-03-06 16:09:24 -0500

    Add screenshot alias

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -5,3 +5,6 @@ alias la 'ls -Fa --color=auto'
 alias lla 'ls -Fla --color=auto'
 alias ping 'ping -c3'
 alias c 'clear'
+function screenshot --wraps scrot --description 'alias screenshot=scrot -e \'mv $f ~/Pictures/Screenshots\''
+    scrot -e 'mv $f ~/Pictures/Screenshots'
+end
